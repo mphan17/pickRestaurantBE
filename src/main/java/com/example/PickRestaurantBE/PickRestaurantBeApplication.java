04ffc449a51081da5411808db15e4225765cf875
Mindi Phan <mphan@allstate.com>
2018-05-26 19:14:13 -0500

    first commit :tada:

---
+++ src/main/java/com/example/PickRestaurantBE/PickRestaurantBeApplication.java
@@ -0,0 +1,12 @@
+package com.example.PickRestaurantBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickRestaurantBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickRestaurantBeApplication.class, args);
+	}
+}
